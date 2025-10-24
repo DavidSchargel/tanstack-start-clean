4b07f35ff2d0e7727669be5bf2819e5b82db1d5c
David Schargel <david@schargel.com>
2025-10-24 09:23:18 -0700

    refactor: lots of cleanups to make this a "clean" TanStack Start project to build from
    - revised README.md to be more helpful
    - added extensive .gitignore
    - fixed ESlint and prettier to work appropriately
    - cleaned up the main index route and header by removing feature descriptions and icons
    - removed demo data and superfluous icons

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,17 @@
 const config = {
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: ['**/*.css'],
+      options: {
+        // Use double quotes in CSS and allow wider lines for long font-family lists
+        singleQuote: false,
+        printWidth: 120,
+      },
+    },
+  ],
+}
 
-export default config;
+export default config
