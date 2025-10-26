ce2d7cf89eaf938b6dff3b8271cdca48dc2416bd
David Schargel <david@schargel.com>
2025-10-26 13:27:05 -0700

    chore: Have ESLint and Prettier ignore auto-generated `src/routeTree.gen.ts`

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import { tanstackConfig } from '@tanstack/eslint-config'
 
 export default [
   {
-    ignores: ['*.config.js'],
+    ignores: ['*.config.js', 'src/routeTree.gen.ts'],
   },
   ...tanstackConfig,
 ]
